399fdbceff51b5a0e2d6b410ebd08615ac1fb8cd
nachiketh 8217 <159506575+nachiketh9-png@users.noreply.github.com>
2025-11-05 11:34:25 +0530

    Create node.c for linked list node structure

---
+++ create a node.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+    int data;
+    struct Node*next;
+};
+struct Node*head=NULL;
+int main(){
+    head=(struct Node*)malloc(sizeof(struct Node));
+    head->data=7;
+    head->next=NULL;
+    printf("node data is %d\n", head->data);
+    return 0;
+}
